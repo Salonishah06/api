d8ef428ffcabe6448a87cf6a0ce05ba99b43c49d
Saloni <salonishah306@gmail.com>
2022-11-19 06:47:47 +0530

    task 1 done

---
+++ src/components/Activities.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+function Activities() {
+    function loadActivity() {
+        setIsLoading(true);
+        fetch("http://www.boredapi.com/api/activity")
+            .then((response) => response.json())
+            .then((data) => {
+                setActivities([...activities,data])
+                setIsLoading(false);
+            });
+            
+    }
+    const [activities, setActivities] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => { loadActivity() },[])
+
+    if(activities.length===0){
+        <p>Loading...</p>
+    }
+    return (
+        <div>
+            <ul>
+                {activities.map((activity)=>{
+                    return <li key={activity.key}>{activity.activity}</li>
+                })}
+            </ul>
+            <button  disabled={isLoading}onClick={loadActivity}>Load Another</button>
+        </div>
+    );
+}
+
+export default Activities;
